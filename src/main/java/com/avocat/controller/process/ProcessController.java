45a364ef100b81d705a4e7e6085af7677d6857d9
Michael <msousacode@gmail.com>
2023-03-15 09:34:05 -0300

    criacao teste processos.

--- src/main/java/com/avocat/controller/process/ProcessController.java
+++ src/main/java/com/avocat/controller/process/ProcessController.java
@@ -1,4 +1,28 @@
 package com.avocat.controller.process;
 
+import com.avocat.persistence.entity.process.Forum;
+import com.avocat.persistence.entity.process.Process;
+import com.avocat.service.ProcessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping(path = "/v1/customer/{customerId}/process", produces = MediaType.APPLICATION_JSON_VALUE)
+@RestController
 public class ProcessController {
+
+    @Autowired
+    private ProcessService processService;
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OWNER') or hasAuthority('LAWYER_WRITE')")
+    @PostMapping
+    public ResponseEntity<Process> create(@PathVariable(value = "customerId", required = true) UUID customerId, @RequestBody Forum forum) {
+        forum.setCustomerId(customerId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(processService.save(forum));
+    }
 }
--- src/test/java/com/avocat/service/ProcessServiceTest.java
+++ src/test/java/com/avocat/service/ProcessServiceTest.java
@@ -5,10 +5,8 @@ import com.avocat.persistence.entity.Contract;
 import com.avocat.persistence.entity.Customer;
 import com.avocat.persistence.entity.process.Area;
 import com.avocat.persistence.entity.process.Process;
-import com.avocat.persistence.types.CompanyTypes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
